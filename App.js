add45cf30da7a56d88ca552ebc61c62ddd19d9fb
yahyayesilyurt <yesilyurty21@itu.edu.tr>
2024-08-23 21:37:49 +0300

    added linear gradient

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
   },
 });
