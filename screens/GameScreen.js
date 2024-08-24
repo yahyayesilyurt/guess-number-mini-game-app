500983c06cad9bdb855101f5cc0f5bf839d46c8b
yahyayesilyurt <yesilyurty21@itu.edu.tr>
2024-08-24 13:58:18 +0300

    Game screen development is started and Title component is added

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,25 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 const GameScreen = () => {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 32,
+  },
+});
